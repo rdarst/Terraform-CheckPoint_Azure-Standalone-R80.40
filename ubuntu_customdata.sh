c6ff62b078e2515046c286a455c282a5212fe484
rdarst <45427115+rdarst@users.noreply.github.com>
2020-03-24 13:17:32 -0400

    Fix refresh

--- ubuntu_customdata.sh
+++ ubuntu_customdata.sh
@@ -8,7 +8,7 @@ until curl \
     sleep 1
  done
  sudo chmod a+w /var/www/html/index.html
- echo "<html><head><meta http-equiv=refresh content=2;'http://cg-demo.MYDNSNAME.INROUTE53/' /> </head><body><center><H1>" > /var/www/html/index.html
+ echo "<html><head></head><body><center><H1>" > /var/www/html/index.html
  echo $HOSTNAME >> /var/www/html/index.html
  echo "<BR><BR>Check Point CloudGuard Terraform Demo <BR><BR>Any Cloud, Any App, Unmatched Security<BR><BR>" >> /var/www/html/index.html
  echo "<img src=\"/CloudGuard.png\" height=\"25%\">" >> /var/www/html/index.html
